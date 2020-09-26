14bd0042f835303a9537d72287be0376059a2b0f
Maciej Gorczyczynski <mgorczyczynski@o2.pl>
2020-09-26 11:49:01 +0200

    Overlappng rectangles, finished problem and started working on visualization

--- ChallengesUI/OverlappingRectanglesView.Designer.cs
+++ ChallengesUI/OverlappingRectanglesView.Designer.cs
@@ -28,24 +28,300 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OverlappingRectanglesView));
             this.BackToAllButton = new System.Windows.Forms.Button();
+            this.DescriptionTexBox = new System.Windows.Forms.TextBox();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.aXTextBox = new System.Windows.Forms.TextBox();
+            this.AxLabel = new System.Windows.Forms.Label();
+            this.AYLabel = new System.Windows.Forms.Label();
+            this.aYTextBox = new System.Windows.Forms.TextBox();
+            this.AWLabel = new System.Windows.Forms.Label();
+            this.aWidthTextBox = new System.Windows.Forms.TextBox();
+            this.AHLabel = new System.Windows.Forms.Label();
+            this.aHeightTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.BHLabel = new System.Windows.Forms.Label();
+            this.bHeightTextBox = new System.Windows.Forms.TextBox();
+            this.BWLabel = new System.Windows.Forms.Label();
+            this.bWidthTextBox = new System.Windows.Forms.TextBox();
+            this.BYLabel = new System.Windows.Forms.Label();
+            this.bYTextBox = new System.Windows.Forms.TextBox();
+            this.BxLabel = new System.Windows.Forms.Label();
+            this.bXTextBox = new System.Windows.Forms.TextBox();
+            this.ALabel = new System.Windows.Forms.Label();
+            this.BLabel = new System.Windows.Forms.Label();
+            this.VisualizationButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BackToAllButton
             // 
-            this.BackToAllButton.Location = new System.Drawing.Point(536, 386);
+            this.BackToAllButton.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.BackToAllButton.Location = new System.Drawing.Point(12, 453);
             this.BackToAllButton.Name = "BackToAllButton";
-            this.BackToAllButton.Size = new System.Drawing.Size(209, 64);
+            this.BackToAllButton.Size = new System.Drawing.Size(209, 45);
             this.BackToAllButton.TabIndex = 0;
             this.BackToAllButton.Text = "Back to all";
             this.BackToAllButton.UseVisualStyleBackColor = true;
             this.BackToAllButton.Click += new System.EventHandler(this.BackToAllButton_Click);
             // 
+            // DescriptionTexBox
+            // 
+            this.DescriptionTexBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DescriptionTexBox.Location = new System.Drawing.Point(24, 77);
+            this.DescriptionTexBox.Multiline = true;
+            this.DescriptionTexBox.Name = "DescriptionTexBox";
+            this.DescriptionTexBox.ReadOnly = true;
+            this.DescriptionTexBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.DescriptionTexBox.Size = new System.Drawing.Size(894, 105);
+            this.DescriptionTexBox.TabIndex = 2;
+            this.DescriptionTexBox.Text = resources.GetString("DescriptionTexBox.Text");
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DescriptionLabel.Location = new System.Drawing.Point(405, 30);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(133, 32);
+            this.DescriptionLabel.TabIndex = 3;
+            this.DescriptionLabel.Text = "Desription:";
+            // 
+            // aXTextBox
+            // 
+            this.aXTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.aXTextBox.Location = new System.Drawing.Point(240, 204);
+            this.aXTextBox.Name = "aXTextBox";
+            this.aXTextBox.Size = new System.Drawing.Size(37, 40);
+            this.aXTextBox.TabIndex = 4;
+            // 
+            // AxLabel
+            // 
+            this.AxLabel.AutoSize = true;
+            this.AxLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.AxLabel.Location = new System.Drawing.Point(184, 207);
+            this.AxLabel.Name = "AxLabel";
+            this.AxLabel.Size = new System.Drawing.Size(50, 35);
+            this.AxLabel.TabIndex = 5;
+            this.AxLabel.Text = "x =";
+            // 
+            // AYLabel
+            // 
+            this.AYLabel.AutoSize = true;
+            this.AYLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.AYLabel.Location = new System.Drawing.Point(305, 207);
+            this.AYLabel.Name = "AYLabel";
+            this.AYLabel.Size = new System.Drawing.Size(51, 35);
+            this.AYLabel.TabIndex = 7;
+            this.AYLabel.Text = "y =";
+            // 
+            // aYTextBox
+            // 
+            this.aYTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.aYTextBox.Location = new System.Drawing.Point(362, 204);
+            this.aYTextBox.Name = "aYTextBox";
+            this.aYTextBox.Size = new System.Drawing.Size(37, 40);
+            this.aYTextBox.TabIndex = 6;
+            // 
+            // AWLabel
+            // 
+            this.AWLabel.AutoSize = true;
+            this.AWLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.AWLabel.Location = new System.Drawing.Point(427, 207);
+            this.AWLabel.Name = "AWLabel";
+            this.AWLabel.Size = new System.Drawing.Size(105, 35);
+            this.AWLabel.TabIndex = 9;
+            this.AWLabel.Text = "Width =";
+            // 
+            // aWidthTextBox
+            // 
+            this.aWidthTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.aWidthTextBox.Location = new System.Drawing.Point(538, 204);
+            this.aWidthTextBox.Name = "aWidthTextBox";
+            this.aWidthTextBox.Size = new System.Drawing.Size(37, 40);
+            this.aWidthTextBox.TabIndex = 8;
+            // 
+            // AHLabel
+            // 
+            this.AHLabel.AutoSize = true;
+            this.AHLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.AHLabel.Location = new System.Drawing.Point(603, 207);
+            this.AHLabel.Name = "AHLabel";
+            this.AHLabel.Size = new System.Drawing.Size(113, 35);
+            this.AHLabel.TabIndex = 11;
+            this.AHLabel.Text = "Height =";
+            // 
+            // aHeightTextBox
+            // 
+            this.aHeightTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.aHeightTextBox.Location = new System.Drawing.Point(722, 204);
+            this.aHeightTextBox.Name = "aHeightTextBox";
+            this.aHeightTextBox.Size = new System.Drawing.Size(37, 40);
+            this.aHeightTextBox.TabIndex = 10;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.outputTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.outputTextBox.Location = new System.Drawing.Point(460, 406);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(144, 40);
+            this.outputTextBox.TabIndex = 12;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.OutputLabel.Location = new System.Drawing.Point(330, 409);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(124, 35);
+            this.OutputLabel.TabIndex = 13;
+            this.OutputLabel.Text = "Output = ";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.CalculateButton.Location = new System.Drawing.Point(336, 333);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(268, 47);
+            this.CalculateButton.TabIndex = 14;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // BHLabel
+            // 
+            this.BHLabel.AutoSize = true;
+            this.BHLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.BHLabel.Location = new System.Drawing.Point(603, 269);
+            this.BHLabel.Name = "BHLabel";
+            this.BHLabel.Size = new System.Drawing.Size(113, 35);
+            this.BHLabel.TabIndex = 22;
+            this.BHLabel.Text = "Height =";
+            // 
+            // bHeightTextBox
+            // 
+            this.bHeightTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.bHeightTextBox.Location = new System.Drawing.Point(722, 266);
+            this.bHeightTextBox.Name = "bHeightTextBox";
+            this.bHeightTextBox.Size = new System.Drawing.Size(37, 40);
+            this.bHeightTextBox.TabIndex = 21;
+            // 
+            // BWLabel
+            // 
+            this.BWLabel.AutoSize = true;
+            this.BWLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.BWLabel.Location = new System.Drawing.Point(427, 269);
+            this.BWLabel.Name = "BWLabel";
+            this.BWLabel.Size = new System.Drawing.Size(105, 35);
+            this.BWLabel.TabIndex = 20;
+            this.BWLabel.Text = "Width =";
+            // 
+            // bWidthTextBox
+            // 
+            this.bWidthTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.bWidthTextBox.Location = new System.Drawing.Point(538, 266);
+            this.bWidthTextBox.Name = "bWidthTextBox";
+            this.bWidthTextBox.Size = new System.Drawing.Size(37, 40);
+            this.bWidthTextBox.TabIndex = 19;
+            // 
+            // BYLabel
+            // 
+            this.BYLabel.AutoSize = true;
+            this.BYLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.BYLabel.Location = new System.Drawing.Point(305, 269);
+            this.BYLabel.Name = "BYLabel";
+            this.BYLabel.Size = new System.Drawing.Size(51, 35);
+            this.BYLabel.TabIndex = 18;
+            this.BYLabel.Text = "y =";
+            // 
+            // bYTextBox
+            // 
+            this.bYTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.bYTextBox.Location = new System.Drawing.Point(362, 266);
+            this.bYTextBox.Name = "bYTextBox";
+            this.bYTextBox.Size = new System.Drawing.Size(37, 40);
+            this.bYTextBox.TabIndex = 17;
+            // 
+            // BxLabel
+            // 
+            this.BxLabel.AutoSize = true;
+            this.BxLabel.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.BxLabel.Location = new System.Drawing.Point(184, 269);
+            this.BxLabel.Name = "BxLabel";
+            this.BxLabel.Size = new System.Drawing.Size(50, 35);
+            this.BxLabel.TabIndex = 16;
+            this.BxLabel.Text = "x =";
+            // 
+            // bXTextBox
+            // 
+            this.bXTextBox.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.bXTextBox.Location = new System.Drawing.Point(240, 266);
+            this.bXTextBox.Name = "bXTextBox";
+            this.bXTextBox.Size = new System.Drawing.Size(37, 40);
+            this.bXTextBox.TabIndex = 15;
+            // 
+            // ALabel
+            // 
+            this.ALabel.AutoSize = true;
+            this.ALabel.Location = new System.Drawing.Point(17, 203);
+            this.ALabel.Name = "ALabel";
+            this.ALabel.Size = new System.Drawing.Size(163, 37);
+            this.ALabel.TabIndex = 23;
+            this.ALabel.Text = "Rectangle A:";
+            // 
+            // BLabel
+            // 
+            this.BLabel.AutoSize = true;
+            this.BLabel.Location = new System.Drawing.Point(17, 265);
+            this.BLabel.Name = "BLabel";
+            this.BLabel.Size = new System.Drawing.Size(161, 37);
+            this.BLabel.TabIndex = 24;
+            this.BLabel.Text = "Rectangle B:";
+            // 
+            // VisualizationButton
+            // 
+            this.VisualizationButton.Font = new System.Drawing.Font("Segoe UI", 18.25F);
+            this.VisualizationButton.Location = new System.Drawing.Point(664, 400);
+            this.VisualizationButton.Name = "VisualizationButton";
+            this.VisualizationButton.Size = new System.Drawing.Size(165, 50);
+            this.VisualizationButton.TabIndex = 25;
+            this.VisualizationButton.Text = "Visualization";
+            this.VisualizationButton.UseVisualStyleBackColor = true;
+            this.VisualizationButton.Click += new System.EventHandler(this.VisualizationButton_Click);
+            // 
             // OverlappingRectanglesView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(881, 475);
+            this.ClientSize = new System.Drawing.Size(942, 510);
+            this.Controls.Add(this.VisualizationButton);
+            this.Controls.Add(this.BLabel);
+            this.Controls.Add(this.ALabel);
+            this.Controls.Add(this.BHLabel);
+            this.Controls.Add(this.bHeightTextBox);
+            this.Controls.Add(this.BWLabel);
+            this.Controls.Add(this.bWidthTextBox);
+            this.Controls.Add(this.BYLabel);
+            this.Controls.Add(this.bYTextBox);
+            this.Controls.Add(this.BxLabel);
+            this.Controls.Add(this.bXTextBox);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.AHLabel);
+            this.Controls.Add(this.aHeightTextBox);
+            this.Controls.Add(this.AWLabel);
+            this.Controls.Add(this.aWidthTextBox);
+            this.Controls.Add(this.AYLabel);
+            this.Controls.Add(this.aYTextBox);
+            this.Controls.Add(this.AxLabel);
+            this.Controls.Add(this.aXTextBox);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.DescriptionTexBox);
             this.Controls.Add(this.BackToAllButton);
             this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
@@ -53,11 +329,36 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Overlapping Rectangles";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button BackToAllButton;
+        private System.Windows.Forms.TextBox DescriptionTexBox;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.TextBox aXTextBox;
+        private System.Windows.Forms.Label AxLabel;
+        private System.Windows.Forms.Label AYLabel;
+        private System.Windows.Forms.TextBox aYTextBox;
+        private System.Windows.Forms.Label AWLabel;
+        private System.Windows.Forms.TextBox aWidthTextBox;
+        private System.Windows.Forms.Label AHLabel;
+        private System.Windows.Forms.TextBox aHeightTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label BHLabel;
+        private System.Windows.Forms.TextBox bHeightTextBox;
+        private System.Windows.Forms.Label BWLabel;
+        private System.Windows.Forms.TextBox bWidthTextBox;
+        private System.Windows.Forms.Label BYLabel;
+        private System.Windows.Forms.TextBox bYTextBox;
+        private System.Windows.Forms.Label BxLabel;
+        private System.Windows.Forms.TextBox bXTextBox;
+        private System.Windows.Forms.Label ALabel;
+        private System.Windows.Forms.Label BLabel;
+        private System.Windows.Forms.Button VisualizationButton;
     }
 }
--- ChallengesUI/OverlappingRectanglesView.cs
+++ ChallengesUI/OverlappingRectanglesView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChallengesUI.PopUp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,12 +18,94 @@ namespace ChallengesUI
             InitializeComponent();
         }
 
+        public int[] AParams { get; set; }
+        public int[] BParams { get; set; }
+
         private void BackToAllButton_Click(object sender, EventArgs e)
         {
             this.Hide();
             MainView frm = new MainView();
             frm.ShowDialog();
             this.Close();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if (DataIsValid())
+            {
+                int aX = Math.Max(AParams[0], BParams[0]);
+                int bX = Math.Min(AParams[0] + AParams[2], BParams[0] + BParams[2]);
+                int aY = Math.Max(AParams[1], BParams[1]);
+                int bY = Math.Min(AParams[1] + AParams[3], BParams[1] + BParams[3]);
+
+                if (bX >= aX && bY >= aY)
+                {
+                    int output = (bX - aX) * (bY - aY);
+                    outputTextBox.Text = output.ToString();
+                }
+                else
+                {
+                    outputTextBox.Text = "0";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct values: integers");
+            }
+        }
+
+        private bool DataIsValid()
+        {
+            bool output = true;
+
+            if (int.TryParse(aXTextBox.Text, out int aX) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(aYTextBox.Text, out int aY) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(aWidthTextBox.Text, out int aWidth) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(aHeightTextBox.Text, out int aHeight) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(bXTextBox.Text, out int bX) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(bYTextBox.Text, out int bY) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(bWidthTextBox.Text, out int bWidth) == false)
+            {
+                output = false;
+            }
+            if (int.TryParse(bHeightTextBox.Text, out int bHeight) == false)
+            {
+                output = false;
+            }
+
+            if (output == true)
+            {
+                int[] arrA = { aX, aY, aWidth, aHeight };
+                AParams = arrA;
+                int[] arrB = { bX, bY, bWidth, bHeight };
+                BParams = arrB;
+            }
+
+            return output;
+        }
+
+        private void VisualizationButton_Click(object sender, EventArgs e)
+        {
+            OverlappingRectanglesChart frm = new OverlappingRectanglesChart();
+            frm.Show();
         }
     }
 }
