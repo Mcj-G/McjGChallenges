a28abc786fc4ccc22026f0dffed73488bd532cfb
Maciej Gorczyczynski <mgorczyczynski@o2.pl>
2020-10-11 10:11:35 +0200

    Working in progress on Binary tree

---
+++ ChallengesUI/Helpers/GBTDataTypesList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengesUI.Helpers
+{
+    public class GBTDataTypesList
+    {
+        public List<string> TypeNames { get; set; }
+        public List<string> Order { get; set; }
+        public List<string> OrderType { get; set; }
+
+        public List<int> IntegerList { get; set; }
+        public List<string> StringList { get; set; }
+        public List<DateTime> DateList { get; set; }
+
+
+        public GBTDataTypesList()
+        {
+            TypeNames = new List<string>
+            {
+                "integer",
+                "text"
+            };
+            Order = new List<string>
+            {
+                "ascending",
+                "descending"
+            };
+            OrderType = new List<string>
+            {
+                "alphabetical",
+                "length"
+            };
+        }
+    }
+}
